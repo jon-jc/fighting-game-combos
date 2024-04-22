b0d3fa8459e357800a7ab3e31e3ee0b81d99302f
jon-jc <jonathancho.jc@gmail.com>
2024-04-22 12:38:35 -0700

    Add Characters page

---
+++ app/characters/page.tsx
@@ -0,0 +1,14 @@
+import Character from "@/components/character";
+import HeaderPage from "@/components/header";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <HeaderPage />
+      <Character />
+    </>
+  );
+};
+
+export default page;
--- components/header.tsx
+++ components/header.tsx
@@ -12,7 +12,7 @@ const HeaderPage = () => {
       <nav className="ml-auto flex gap-4 sm:gap-6">
         <Link
           className="text-sm font-medium hover:underline underline-offset-4"
-          href="#"
+          href="/characters"
         >
           Characters
         </Link>
